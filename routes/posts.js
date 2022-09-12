3f3be5da15e2a8af97cdf4a6ef2f8c71f0803b63
ZZAME <92978598+DEVZZAME@users.noreply.github.com>
2022-09-13 04:37:20 +0900

    modify posts.js

--- routes/posts.js
+++ routes/posts.js
@@ -2,33 +2,42 @@ const express = require('express');
 
 const router = express.Router();
 
-const POSTS = [
-  {
-    title: 'title01',
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  },
-  {
-    title: 'title02',
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  },
-  {
-    title: 'title03',
-    content:
-      "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.",
-  },
-];
+const mongoClient = require('./mongo');
 
-router.get('/', (req, res) => {
+async function getPosts() {
+  const client = await mongoClient.connect();
+  const cursor = client.db('kdt1').collection('post').find();
+  const data = await cursor.toArray();
+  return data;
+}
+
+async function updatePosts(data) {
+  const client = await mongoClient.connect();
+  const cursor = client.db('kdt1').collection('post');
+  await cursor.deleteMany({});
+  await cursor.insertMany(data);
+  const dbs = client.db('kdt1').collection('post').find();
+  const result = await dbs.toArray();
+  return result;
+}
+
+// POSTS 글목록 페이지
+router.get('/', async (req, res) => {
+  const POSTS = await getPosts();
   res.render('posts', { POSTS });
 });
 
-router.get('/:title', (req, res) => {
+router.get('/add', async (req, res) => {
+  const POSTS = await getPosts();
+  res.render('posts_add.ejs', { POSTS });
+});
+
+// POSTS 상세 페이지
+router.get('/:title', async (req, res) => {
+  const POSTS = await getPosts();
   const postData = POSTS.find((post) => post.title === req.params.title);
   if (postData) {
-    // ejs 페이지 만들어서 보여주기
-    res.send(postData);
+    res.render('posts_detail.ejs', { POSTS: postData });
   } else {
     const err = new Error('ID not found');
     err.statusCode = 404;
@@ -36,14 +45,25 @@ router.get('/:title', (req, res) => {
   }
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
+  const client = await mongoClient.connect();
+  // 글번호
+  const count = await client
+    .db('kdt1')
+    .collection('counter')
+    .findOne({ name: 'numberOfPosts' });
+  const totalPost = count.totalPost;
+  // 게시물
+  const POSTS = await getPosts();
   if (Object.keys(req.query).length >= 1) {
     if (req.query.title && req.query.content) {
       const newPost = {
+        _id: totalPost + 1,
         title: req.query.title,
         content: req.query.content,
       };
       POSTS.push(newPost);
+      await updatePosts(POSTS);
       res.redirect('/posts');
     } else {
       const err = new Error('Unexpected query');
@@ -53,40 +73,45 @@ router.post('/', (req, res) => {
   } else if (req.body) {
     if (req.body.title && req.body.content) {
       const newPost = {
+        _id: totalPost + 1,
         title: req.body.title,
         content: req.body.content,
       };
       POSTS.push(newPost);
+      await updatePosts(POSTS);
       res.redirect('/posts');
     }
   }
 });
 
-router.put('/:title', (req, res) => {
-  if (req.query.title && req.query.content) {
-    const postData = POSTS.find((post) => post.title === req.params.title);
-    if (postData) {
-      const arrIndex = POSTS.findIndex(
-        (post) => post.title === req.params.title
-      );
-      const modifyPost = {
-        title: req.query.title,
-        content: req.query.content,
-      };
-      POSTS[arrIndex] = modifyPost;
-      res.send('글 수정 완료 ');
-    }
-  } else {
-    const err = new Error('ID not found');
-    err.statusCode = 404;
-    throw err;
-  }
+router.get('/edit/:_id', async (req, res) => {
+  const POSTS = await getPosts();
+  const postData = POSTS.find((post) => post._id === parseInt(req.params._id));
+  res.render('posts_edit.ejs', { POSTS: postData });
 });
 
-router.delete('/:title', (req, res) => {
+router.post('/edit', async (req, res) => {
+  const client = await mongoClient.connect();
+  // 글번호
+  const count = await client
+    .db('kdt1')
+    .collection('post')
+    .updateOne(
+      { _id: parseInt(req.body.id) },
+      { $set: { title: req.body.title, content: req.body.content } },
+      function (err, result) {
+        console.log(result);
+        res.redirect('/posts');
+      }
+    );
+});
+
+router.delete('/:title', async (req, res) => {
+  const POSTS = await getPosts();
   const arrIndex = POSTS.findIndex((post) => post.title === req.params.title);
   if (arrIndex !== -1) {
     POSTS.splice(arrIndex, 1);
+    const result = await updatePosts(POSTS);
     res.send('글 삭제 완료');
   } else {
     const err = new Error('TITLE not found');
@@ -96,3 +121,22 @@ router.delete('/:title', (req, res) => {
 });
 
 module.exports = router;
+
+// if (req.query.title && req.query.content) {
+//   const postData = POSTS.find((post) => post.title === req.params.title);
+//   if (postData) {
+//     const arrIndex = POSTS.findIndex(
+//       (post) => post.title === req.params.title
+//     );
+//     const modifyPost = {
+//       title: req.query.title,
+//       content: req.query.content,
+//     };
+//     POSTS[arrIndex] = modifyPost;
+//     res.send('글 수정 완료 ');
+//   }
+// } else {
+//   const err = new Error('ID not found');
+//   err.statusCode = 404;
+//   throw err;
+// }
