a582c63c7b3c7a723a8f07436b099b8865f805cf
ZZAME <92978598+DEVZZAME@users.noreply.github.com>
2022-09-13 04:37:13 +0900

    modify mongo.js

--- routes/mongo.js
+++ routes/mongo.js
@@ -1,48 +1,39 @@
-// MongoDB 설정
 const { MongoClient, ServerApiVersion } = require('mongodb');
+
 const uri =
-  // 'mongodb+srv://tetz:qwer1234@cluster0.sdiakr0.mongodb.net/?retryWrites=true&w=majority';
   'mongodb+srv://zzame:zzame1234@cluster0.7b6ewbn.mongodb.net/?retryWrites=true&w=majority';
+
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   serverApi: ServerApiVersion.v1,
 });
 
+module.exports = client;
+
 async function main() {
-  // await client.connect(err);
+  await client.connect();
+
   const users = client.db('kdt1').collection('users');
+
   // await users.deleteMany({});
-  // await users.updateOne({ age: 4 }, { $set: { age: 40 } });
-  // await users.updateMany({ age: { $lte: 5 } }, { $set: { age: 4, old: 'no' } });
-  // await users.deleteMany({
-  //   age: { $gte: 5 },
-  // });
-  // await users.deleteOne({ name: 'pororo' });
-  // await users.insertOne({
-  //   name: 'hansol',
-  //   age: 'idk',
-  // });
   // await users.insertMany([
   //   {
-  //     name: 'pororo',
-  //     age: 5,
+  //     id: 'userId01',
+  //     name: 'User01',
+  //     email: 'user01@blog.com',
   //   },
   //   {
-  //     name: 'loopy',
-  //     age: 6,
+  //     id: 'userId02',
+  //     name: 'User02',
+  //     email: 'user02@blog.com',
   //   },
   //   {
-  //     name: 'crong',
-  //     age: 4,
+  //     id: 'userId03',
+  //     name: 'User03',
+  //     email: 'user03@blog.com',
   //   },
   // ]);
-  const data = users.find({ age: { $gte: 5 } });
-  // const data = users.find({ });
-  // await data.forEach(console.log);
-  const arr = await data.toArray();
-  // console.log(data);
-  console.log(arr);
-  await client.close();
 }
+
 main();
