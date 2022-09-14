6830b2be8d104b72564c1378df92242036838050
ZZAME <92978598+DEVZZAME@users.noreply.github.com>
2022-09-14 17:35:21 +0900

    modified mongo.js

--- routes/client.js
+++
@@ -1,9 +0,0 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
-const uri =
-  'mongodb+srv://zzame:zzame1234@cluster0.7b6ewbn.mongodb.net/?retryWrites=true&w=majority';
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-  serverApi: ServerApiVersion.v1,
-});
-module.exports = client;
